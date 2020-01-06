f28fe793ef1dfaa9b562332c11d49a1dc2b93069
james rutledge <jarutledge2@gmail.com>
2020-01-05 19:39:00 -0500

    update api config

--- api/server/config/config.js
+++ api/server/config/config.js
@@ -2,30 +2,30 @@
 
 module.exports = {
   "development": {
-    "username": "jmz527",
+    "username": "postgres",
     "password": null,
-    "database": "spydr_dev",
-    "host": "127.0.0.1",
+    "database": "postgres",
+    "host": "0.0.0.0",
     "port": 5432,
     "logging": console.log,
     "dialect": "postgres",
     "operatorsAliases": "Sequelize.Op"
   },
   "test": {
-    "username": "jmz527",
+    "username": "postgres",
     "password": null,
-    "database": "spydr_test",
-    "host": "127.0.0.1",
+    "database": "postgres",
+    "host": "0.0.0.0",
     "port": 5432,
     "logging": false,
     "dialect": "postgres",
     "operatorsAliases": "Sequelize.Op"
   },
   "production": {
-    "username": "jmz527",
+    "username": "postgres",
     "password": null,
-    "database": "spydr",
-    "host": "127.0.0.1",
+    "database": "postgres",
+    "host": "0.0.0.0",
     "port": 5432,
     "logging": false,
     "dialect": "postgres",
